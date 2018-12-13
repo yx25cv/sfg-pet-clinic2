3127dc9fb306e1b50dc635d993953927081200a0
yx25cv <42892130+yx25cv@users.noreply.github.com>
2018-12-13 15:33:04 +0100

    Implemented repos. Closes #39

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/PetReository.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Pet;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetReository extends CrudRepository<Pet, Long> {
+
+
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -0,0 +1,8 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialtyRepository extends CrudRepository<Specialty, Long> {
+
+}
